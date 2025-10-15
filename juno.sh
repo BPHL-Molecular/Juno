1411244ad5aa33e539c267fd5e7e49f200fea7ac
Arnold Rodriguez-Hilario <54407020+arodzh-sudo@users.noreply.github.com>
2025-10-15 17:08:00 -0400

    Update juno.sh

--- juno.sh
+++ juno.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #SBATCH --account=bphl-umbrella
 #SBATCH --qos=bphl-umbrella
-#SBATCH --job-name=juno_original_reference
+#SBATCH --job-name=juno_orov_assembly
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=arnold.rodriguezhilario@flhealth.gov
+#SBATCH --mail-user=user@email.com
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100gb
-#SBATCH --time=5:00:00
-#SBATCH --output=juno_original_reference.%j.out
-#SBATCH --error=juno_original_reference.%j.err
+#SBATCH --time=2:00:00
+#SBATCH --output=juno_orov_assembly.%j.out
+#SBATCH --error=juno_orov_assembly.%j.err
 
 # Load required modules
 module load apptainer nextflow python3
 
 # Set up singularity/apptainer cache
-export NXF_SINGULARITY_CACHEDIR=/blue/bphl-florida/arnold.rodriguez/singularity
+export NXF_SINGULARITY_CACHEDIR=/path/to/singularity/cache
 export PATH
 
 # Run nextflow command
 nextflow run juno.nf -profile singularity -params-file params.yaml
-
